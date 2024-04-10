235d2aeeed2d35a738197bb01a2b4528779f8dfb
infinity0807 <143962672+infinity0807@users.noreply.github.com>
2024-04-10 19:07:48 -0400

    Add files via upload

---
+++ js/challenge1.js
@@ -0,0 +1,18 @@
+
+
+document.addEventListener("dblclick", function() {
+    time = new Date();
+    alert(time);
+});
+
+
+document.querySelector('#toggle').addEventListener("click", function() {
+    document.querySelector('#emailBox').classList.toggle('hidden');
+
+
+    
+  
+  
+   
+  }
+);
